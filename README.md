e38c6fd642abc8e14c2c776722e37fc2c1207649
Ameet <bharwani@gmail.com>
2022-03-11 14:10:13 -0500

    First commit
    Signed-off-by: Ameet <bharwani@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# ld
+# LaunchDarkly Testing
+
+**Built using Client-side NodeJS SDK**<br>
+Assume node is already installed. Otherwise install node. 
+## Build instructions
+
+1. Install the LaunchDarkly Client-Side NodeJS SDK by running `npm init` into a directory.
+
+2. Run `npm install launchdarkly-node-client-sdk@2.0.2 --save`
+
+3. Edit `index.js` and the client-side ID in LD SDK's initialize():
+
+```js
+  const ldClient = LaunchDarkly.initialize("<CLIENT-ID></CLIENT-ID>", user);
+```
+
+4. Run the app using:
+```
+  node index.js 
+```
+
+### Thank you
---
+++ index.js
@@ -0,0 +1,68 @@
+// Import the LaunchDarkly client
+var LaunchDarkly = require("launchdarkly-node-client-sdk");
+
+// Enable Logger in debug mode
+// TODO: I didn't get this working before running out of time
+const config = {
+  logger: LaunchDarkly.basicLogger({
+    level: "debug",
+    destination: console.log,
+  }),
+};
+
+// Set up user properties. 2 users so I can test Targeted flags
+// var user = {
+//   key: "example-user-key",
+//   email: "sed@bed.org",
+// };
+
+var user = {
+  key: "examplef-user-key",
+  email: "sed2@bed.org",
+};
+
+// Create a single instance of the LaunchDarkly client
+const ldClient = LaunchDarkly.initialize("<CLIENT-ID>", user);
+
+// Function to log messages to console
+function showMessage(s) {
+  console.log("*** " + s);
+}
+
+ldClient
+  .waitForInitialization()
+  .then(function () {
+    showMessage("SDK successfully initialized!" + "\n");
+    //const flagValue = ldClient.variation("pricing-tier-3", false);
+    let allFlagsResult = ldClient.allFlags();
+
+    showMessage("Output all flags for user");
+    for (const [key, value] of Object.entries(allFlagsResult)) {
+      showMessage(`${key}: ${value}`);
+    }
+    showMessage("");
+
+    // Can't do this as allFlagsResult is a Map
+    //showMessage("Flags" + allFlagsResult.entries());
+
+
+    showMessage("Now take action on  'true' flags for this user");
+    for (const [key, value] of Object.entries(allFlagsResult)) {
+       if (value == true){
+            showMessage("Feature flag " + `${key}:` + " is " + `${value}` + " for this user")
+            showMessage("STUB: Expose related functionality to user!");
+        }
+    }
+    showMessage("");
+
+    // Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
+    // events to LaunchDarkly before the program exits. If analytics events are not delivered,
+    // the user properties and flag usage statistics will not appear on your dashboard. In a
+    // normal long-running application, the SDK would continue running and events would be
+    // delivered automatically in the background.
+    ldClient.close();
+  })
+  .catch(function (error) {
+    showMessage("SDK failed to initialize: " + error);
+    process.exit(1);
+  });
